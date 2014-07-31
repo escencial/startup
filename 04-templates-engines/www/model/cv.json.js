470a601db3b679191873915de0c14540cd681901
salvadorp <escencial@escencia.local>
2014-07-31 20:25:04 -0300

    finnish handlebars engine

---
+++ 04-templates-engines/www/model/cv.json.js
@@ -0,0 +1,47 @@
+var cvJson = {
+
+	"image_url": "https://yt3.ggpht.com/-PFtk0srxnxg/AAAAAAAAAAI/AAAAAAAAAAA/8zxbvGgAm-0/s900-c-k-no/photo.jpg",
+
+	"personal_info": {
+		"name" : "salvador",
+		"lastname": "palmiciano",
+		"phone": "4545 1718",
+		"description": "Salvador began developing in VB6, 4 years later he did some web projects using PHP language in which he acquire more knowledge in mysql, js, css, oop, design patterns, frameworks and work methodologies. He worked free lance and for some companies before entering Globant in 2014."
+	},
+
+	"study_title": "estudios",
+	"study": [
+		{"title":"ESBA Flores Buenos Aires, Argentina ", "content": "High School"}
+	],
+
+	"experience_title": "experiencia",
+	"experience": [
+		{
+			"title":"Netlabs, Buenos Aires, Argentina", 
+			"content": "Maintaining a proprietary ISP management product. Projects telephony and other systems."
+		},
+		{
+			"title":"Tecnosofware, Buenos Aires, Argentina", 
+			"content": "Web application development in these technologies receiving direct client requirements"
+		},
+		{
+			"title":"BDS Consultores, Buenos Aires, Argentina", 
+			"content": "Solution design, implementation and maintenance of intelligent software invoice capture (ReadSoft)."
+		},
+	],
+	"skils": [
+		"php5 – oop – html5 – css3",
+		"zend framework – silex simfony",
+		"mssql - mysql - redbean",
+		"JQuery – bootstrap - ext.js",
+		"svn – git",
+		"hudson – mantis",
+		"SL110 – J2SE (2006)",
+		"SL275 – J2SE (2006)"
+	],
+	"languaje": [
+		"Spanish (native speaker)",
+		"English (intermediate)",
+		"Portuguese (basic)"
+	]
+}
